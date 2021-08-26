5befdbf54d23162261c4bdaa7acfc41e71c3f16f
Andrii Taranenko <archibog2017@gmail.com>
2021-08-26 15:39:32 +0200

    Some

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect } from "react";
+import { NextPage } from "next";
 
 import { TAdditional, AppContext, setAdditionals } from "~/store";
 import useTranslation from "~/intl/useTranslation";
-import { Banner, Cart, Layout } from "~/components";
+import { Banner, Cart, Layout, TBanner } from "~/components";
 
 interface TProps {
   additionals: TAdditional[];
+  hero: TBanner[];
 }
 
-const CartPage: React.FC<TProps> = ({ additionals }) => {
+const CartPage: NextPage<TProps> = ({ additionals, hero }) => {
   const { dispatch } = useContext(AppContext);
   const { t } = useTranslation();
   const cartTitle = t("cartTitle");
@@ -19,20 +21,32 @@ const CartPage: React.FC<TProps> = ({ additionals }) => {
 
   return (
     <Layout title={cartTitle}>
-      <Banner hero={[]} inner />
+      <Banner hero={hero} inner />
       <Cart />
     </Layout>
   );
 };
 
-export const getStaticProps = async () => {
+CartPage.getInitialProps = async () => {
   const response = await fetch(
     "https://sushi-admin.herokuapp.com/additionals?_sort=published_at:ASC"
   );
   const additionals = await response.json();
+  const hero = await Promise.all([
+    fetch("https://sushi-admin.herokuapp.com/hero?_locale=cs"),
+    fetch("https://sushi-admin.herokuapp.com/hero?_locale=ru"),
+  ])
+    .then(async ([cz, ru]) => {
+      const heroCZ = await cz.json();
+      const heroRU = await ru.json();
+
+      return [heroCZ, heroRU];
+    })
+    .then((responseText) => responseText);
 
   return {
-    props: { additionals, revalidate: 10 },
+    additionals,
+    hero,
   };
 };
 
