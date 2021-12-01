7626e132ef70795bc9a42e74be746d46e1ac407d
megathrone777 <krol2006@bigmir.net>
2021-12-01 23:40:51 +0100

    Some changes

--- pages/api/cart/order.ts
+++ pages/api/cart/order.ts
@@ -29,7 +29,7 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
     paymentType,
   } = request.body;
 
-  if (paymentType === "cash" || paymentType === "cardPickup") {
+  if (paymentType === "cash") {
     telegramSendMessage(
       `Заказ №${orderId}
     ${products.map(
@@ -83,11 +83,85 @@ const handler = async (request: NextApiRequest, response: NextApiResponse) => {
         : "Бесплатная"
     }
     \n <b>Email:</b> ${email}
-    \n <b>Тип оплаты:</b> ${
-      paymentType === "cash" ? "Наличными" : "Картой на месте"
-    }
+    \n <b>Тип оплаты:</b> Наличными
     \n <b>Цена:</b> ${orderPrice}Kč
-    \n <a href="tel:${phone.replace(" ", "")}">${phone.replace(" ", "")} ${name}</a>
+    \n <a href="tel:${phone.replace(" ", "")}">${phone.replace(
+        " ",
+        ""
+      )} ${name}</a>
+    \n ${address !== null && address.length > 0 ? "" : "Самовывоз"}
+    `,
+      () => {
+        if (address) {
+          telegramSendMessage(`${address}`);
+        }
+      }
+    );
+
+    response.send({ redirect: "/orderConfirmed", statusCode: 0 });
+    return;
+  }
+
+  if (paymentType === "cardPickup") {
+    telegramSendMessage(
+      `Заказ №${orderId}
+    ${products.map(
+      ({
+        product_modifiers,
+        product_submodifiers,
+        title,
+        quantity,
+      }): string => {
+        const modifiers =
+          (product_modifiers &&
+            !!product_modifiers.length &&
+            product_modifiers.map(
+              ({ price, name: modifier_name }, index: number): string => {
+                const modifier = `\n<b>-${modifier_name} ${price}Kč</b>`;
+                const modifier_submodifiers = product_submodifiers.filter(
+                  ({ modifierIndex }) => modifierIndex === index
+                );
+                const submodifiers = modifier_submodifiers.map(
+                  ({ name: submodifier_name }): string => {
+                    return `\n--<b>${submodifier_name}</b>`;
+                  }
+                );
+
+                return modifier + submodifiers;
+              }
+            )) ||
+          "";
+
+        return `\n<b>${title} ${
+          quantity !== 1 ? `x${quantity}` : ""
+        }</b>${modifiers}`;
+      }
+    )}
+    ${
+      additionals && !!additionals.length
+        ? `
+      ${additionals.map(({ title, quantity }): string => {
+        return `\n--<b>${title} x${quantity}</b>`;
+      })}
+    `
+        : ""
+    }
+    ${note && note.length > 0 ? `\n${note}` : ""}
+    \n <b>Приборы:</b> ${cutleryAmount}
+    \n <b>Доставка:</b> ${
+      deliveryPrice >= 50 && deliveryPrice < 100
+        ? "Обычная"
+        : deliveryPrice >= 100
+        ? "Повышенная"
+        : "Бесплатная"
+    }
+    \n <b>Email:</b> ${email}
+    \n <b>Тип оплаты:</b> Картой на месте
+    \n <b>Цена:</b> ${orderPrice}Kč
+    \n <a href="tel:${phone.replace(" ", "")}">${phone.replace(
+        " ",
+        ""
+      )} ${name}</a>
     \n ${address !== null && address.length > 0 ? "" : "Самовывоз"}
     `,
       () => {
