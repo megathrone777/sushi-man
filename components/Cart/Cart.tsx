d48b94714356cd90ad3bdae5dfba83136d676445
Andrew Taranenko <krol2006@bigmir.net>
2021-12-02 13:06:10 +0100

    Update Cart.tsx

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -328,6 +328,15 @@ const Cart: React.FC = () => {
         });
         return;
       }
+      
+      if (paymentType === "cardPickup") {
+        createOrderByCash({
+          variables: {
+            createOrderInput,
+          },
+        });
+        return;
+      }
 
       createOrderByCard({
         variables: {
