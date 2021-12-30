1238d462e90cff863375a309d0f1678553d9b0e0
megathrone777 <krol2006@bigmir.net>
2021-12-30 09:38:48 +0100

    Some changes

--- src/components/Header/Logo/Logo.tsx
+++ src/components/Header/Logo/Logo.tsx
@@ -6,33 +6,6 @@ import { StyledWrapper, StyledLink } from "./styled";
 
 const Logo: React.FC = () => (
   <StyledWrapper>
-    <div
-      style={{
-        display: "flex",
-        position: "fixed",
-        justifyContent: "center",
-        zIndex: 200,
-        width: '100%',
-        left: 0,
-        right: 0
-      }}
-    >
-      <span
-        style={{
-          fontSize: "200px",
-          zIndex: 200,
-        }}
-      >
-        &#129314;
-      </span>
-      <span
-        style={{
-          fontSize: "200px",
-        }}
-      >
-        &#129326;
-      </span>
-    </div>
     <Link href="/" passHref>
       <StyledLink>
         <Image alt="Logo" layout="fill" src="/images/logo_img.png" />
