fa2dd503eb52841a8cf75bf39e44dd2b0901f8e9
megathrone777 <krol2006@bigmir.net>
2022-01-04 23:15:11 +0100

    Some changes

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -14,6 +14,7 @@ const defaultOptions: DefaultOptions = {
 const client = new ApolloClient({
   uri: "https://sushi-admin.herokuapp.com/graphql",
   headers: {
+    "Access-Control-Allow-Origin": "*",
     authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjQxMzI5NjI5LCJleHAiOjE2NDM5MjE2Mjl9.oebWp8uDoPzNYd_hi1ZdHUvbbgubtDdWvERBaNpXgkg`,
   },
   credentials: 'include',
