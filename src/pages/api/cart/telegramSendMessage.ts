947d4c522c37e30f65fad0c4750007bc8cb9c1be
megathrone777 <krol2006@bigmir.net>
2022-01-06 04:35:52 +0100

    Some changes

--- src/pages/api/cart/telegramSendMessage.ts
+++ src/pages/api/cart/telegramSendMessage.ts
@@ -2,7 +2,7 @@ import { TelegramClient } from "messaging-api-telegram";
 import { ParseMode } from "messaging-api-telegram/dist/TelegramTypes";
 
 const accessToken = "2135558519:AAHY1FPlgWls7tt5b5PSD1q9MkQ2IoahtXc";
-const chatId = "-727652121";
+const chatId = "-470404095";
 const telegram = new TelegramClient({ accessToken });
 
 const telegramSendMessage = (body: string, callback?: () => void): void => {
